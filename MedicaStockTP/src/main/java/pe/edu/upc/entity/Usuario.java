cb99e19161ef7fb4845a54b9222ea63a8fc5842f
mgh989 <mgianella.1999@gmail.com>
2021-05-06 03:47:56 -0500

    Updated

--- MedicaStockTP/src/main/java/pe/edu/upc/entity/Usuario.java
+++ MedicaStockTP/src/main/java/pe/edu/upc/entity/Usuario.java
@@ -25,8 +25,58 @@ public class Usuario {
 	
 	@Column(name="Contraseña", nullable=false, length=30)
 	private String contraseña;
-	
-	
-	
-	
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(int idUsuario, String nombreCompleto, int dni, String correo, String contraseña) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nombreCompleto = nombreCompleto;
+		this.dni = dni;
+		this.correo = correo;
+		this.contraseña = contraseña;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
 }
