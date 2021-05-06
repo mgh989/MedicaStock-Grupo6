079f29db1949e44e2edf6c85a19f4d4c0991d8a1
mgh989 <mgianella.1999@gmail.com>
2021-05-06 03:45:45 -0500

    Updated

--- MedicaStockTP/src/main/java/pe/edu/upc/entity/Usuario.java
+++ MedicaStockTP/src/main/java/pe/edu/upc/entity/Usuario.java
@@ -1,8 +1,32 @@
 package pe.edu.upc.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="usuario")
 public class Usuario {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUsuario;
+	
+	@Column(name="nombreCompleto", nullable=false, length=50)
+	private String nombreCompleto;
+	
+	@Column(name="DNI", nullable=false, length=8)
+	private int dni;
+	
+	@Column(name="Correo", nullable=false, length=50)
+	private String correo;
+	
+	@Column(name="Contraseña", nullable=false, length=30)
+	private String contraseña;
+	
+	
+	
+	
 }
